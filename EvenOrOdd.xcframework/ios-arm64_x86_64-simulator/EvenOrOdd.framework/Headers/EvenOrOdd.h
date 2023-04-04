f0c550297018ab1e57195332e8efc8e4c43ed3cd
George Shoemaker <george.a.shoemaker@gmail.com>
2023-04-04 15:06:18 -0700

    First commit

---
+++ EvenOrOdd.xcframework/ios-arm64_x86_64-simulator/EvenOrOdd.framework/Headers/EvenOrOdd.h
@@ -0,0 +1,18 @@
+//
+//  EvenOrOdd.h
+//  EvenOrOdd
+//
+//  Created by George Shoemaker on 4/4/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EvenOrOdd.
+FOUNDATION_EXPORT double EvenOrOddVersionNumber;
+
+//! Project version string for EvenOrOdd.
+FOUNDATION_EXPORT const unsigned char EvenOrOddVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EvenOrOdd/PublicHeader.h>
+
+
